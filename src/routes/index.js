ad26f639b076752661524b7501c053291ded3914
August Lavrov <august@127.0.0.1>
2020-03-07 01:18:09 +0200

    fixed route ctx props

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,6 @@ const routeSwitch = initRouter({
   [ROUTES.ADMIN]: privated,
 });
 
-export default async (data) => {
-  await routeSwitch(data.ctx.pathname, data.ctx);
+export default async (ctx) => {
+  await routeSwitch(ctx.pathname, ctx);
 };
